a2e3df7f9c0f846f917569f1da97e8e327afd570
EC2 Default User <ec2-user@ip-172-31-12-221.us-east-2.compute.internal>
2018-11-11 16:19:51 +0000

    Just saving work

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,109 @@
+require 'net/http'
+require 'json'
+
+class CharactersController < ApplicationController
+    
+    def new
+    end
+    
+    def index
+        if params[:name1] && params[:name2]
+            lookup_characters(params)
+        end
+    end
+    
+    def show
+    end
+    
+    private
+    
+    def combat
+        @characters.each do |marvel_char|
+        end
+    end
+    
+    def process_battle_word(marvel_char)
+        desc_array = marvel_char.desc.split
+        marvel_char.battle_word = desc_array[marvel_char.seed.to_i]
+        if @winner.nil? && marvel_char.auto_win?
+            @winner = marvel_char
+            puts "#{marvel_char.name} WEENS!"
+        end
+    end
+
+    def process_character(marvel_char, seed)
+        if marvel_char.present?
+            # Get model from existing record or initialize new model
+            existing = Character.find_or_initialize_by(name: marvel_char.name)
+
+            # Update the fields of the existing model from the parsed model.  There might be a 
+            # more efficient way to do this.
+            existing.seed = seed
+            existing.marvel_id = marvel_char.marvel_id
+            existing.desc = marvel_char.desc
+            
+            # Save to invoke the validation mechanism
+            existing.save
+            
+            # Figure out the battle word
+            process_battle_word(existing)
+            
+            @characters << existing
+        end
+    end
+    
+    # Look up both Marvel characters
+    def lookup_characters(params)
+        @characters = Array.new
+        character_result = marvel_lookup(params[:name1])
+        process_character(character_result,  params[:seed])
+        character_result = marvel_lookup(params[:name2])
+        process_character(character_result,  params[:seed])
+    end
+    
+    # Look up a Marvel character by name, using keys, timestamp, and generated
+    # hash number.
+    def marvel_lookup(name)
+        public_key = 'd7625a1b12e996ec43328e82331a3176'
+        private_key = '2aa476235a3bfc3f78c873a80cd88ef1669d58a8'
+        marvel_char = nil
+        
+        begin
+            uri = URI('http://gateway.marvel.com/v1/public/characters')
+            params = Hash.new
+            params["apikey"] = public_key
+            params["ts"] = ts
+            params["hash"] = request_hash(public_key,private_key)
+            params["name"] = name
+            uri.query = URI.encode_www_form(params)
+         
+            res = Net::HTTP.get_response(uri)
+
+            # puts "response #{res.body}"
+            body_h = JSON.parse(res.body)
+            #puts body_h
+            res_arr = body_h["data"]["results"]
+            #puts res_arr
+            res_arr.each do | char_item |
+                #puts char_item
+                marvel_char = Character.new
+                marvel_char.marvel_id = char_item["id"]
+                marvel_char.name = char_item["name"]
+                marvel_char.desc = char_item["description"]
+            end
+        rescue => e
+            puts "failed #{e}"
+        end
+        marvel_char
+    end
+    
+    def request_hash(public_key,private_key)
+        Digest::MD5.hexdigest("#{ts}#{private_key}#{public_key}")
+    end
+
+    def ts
+        begin
+            Time.now.to_i
+        end
+    end
+end
---
+++ app/models/character.rb
@@ -0,0 +1,9 @@
+class Character < ApplicationRecord
+    attr_accessor :marvel_id, :name, :desc, :seed, :battle_word
+    validates :seed, presence: true
+    validates :seed, numericality: { only_integer: true, greater_than: 0, less_than: 10 }
+    
+    def auto_win?
+        battle_word.casecmp?("gamma") || battle_word.casecmp?("radioactive")
+    end
+end
